02129ac054fa4f5a3aaab005d98df72b2504ee5f
Anas Mohamed <amohamed2479@gmail.com>
2021-08-14 13:24:09 -0500

    use a different adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 /** @type {import('@sveltejs/kit').Config} */
 
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
